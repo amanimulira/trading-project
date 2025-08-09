c6af34c0306886783b14ecdd91feea6c446dde43
amanimulira <amanimulira@gmail.com>
2025-08-09 15:38:05 +0100

    major refactors - analysis, data, strategy

--- risk_management/risk_management.py
+++
@@ -1,5 +0,0 @@
-def apply_risk_management(data):
-    """Apply risk managment with stop-loss and position sizing."""
-    data['Stop_Loss'] = data['Close'] * 0.95 # 5% stop-loss
-    data['Position_Size'] = 0.03 # 3% of portfolio per trade
-    return data
